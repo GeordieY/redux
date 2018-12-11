0574a52640b49d16238c64b2a64c77df034e60a1
GeordieY <ghyoungiv@gmail.com>
2018-12-11 09:21:51 -0500

    First commit

---
+++ data_server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var fs = require('fs');
+var favicon = require('serve-favicon');
+
+
+var app = express();
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/public/images/logo.png'));
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('Server started at '+ new Date()+', on port ' + port+'!');
+});
+
+app.get('/', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('index');
+});
+
+app.get('/login', function(request, response){
+  var user_data={
+      name: request.query.player_name
+  };
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('game', {user:user_data});
+});
+
+app.get('/:user/results', function(request, response){
+  var user_data={
+      name: request.params.user,
+      weapon: request.query.weapon
+  };
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.send(JSON.stringify(user_data));
+});
+
+app.get('/rules', function(request, response){
+  //load the csv
+  var users_file = fs.readFileSync('data/users.csv','utf8');
+  console.log(users_file);
+  //parse the csv
+  var rows = users_file.split("\n");
+  console.log(rows);
+  for(var)
+
+
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('rules');
+});
+
+app.get('/stats', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('stats');
+});
+app.get('/about', function(request, response){
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render('about');
+});
